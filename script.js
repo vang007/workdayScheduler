60f8c35b1e5452fc2feceed110059c8e1fdff6eb
vang007 <https://github.com/vang007>
2021-10-11 23:55:58 -0500

    third update

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var dt = new Date();
-document.getElementById('date-time').innerHTML=dt;
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getMonth()+'-'+(today.getDate())+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 
-// var input = document.getElementById("input");
-// input.addEventListener("keyup", function(event) {
-//     if (event.keyCode === 13) {
-//         event.preventDefault();
-//         document.getElementById("myBtn").click();
-//     }
-// }
-// )
+document.getElementById("displayDateTime").innerHTML = daylist[day] + ' <br>   ' + dateTime;
+
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
