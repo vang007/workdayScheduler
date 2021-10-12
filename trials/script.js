12dfe795a3139dc79cd0b4291ec27fdd25a69eba
vang007 <https://github.com/vang007>
2021-10-11 22:36:35 -0500

    updated

---
+++ trials/script.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getMonth()+'-'+(today.getDate())+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+document.getElementById("displayDateTime").innerHTML = daylist[day] + ' <br>   ' + dateTime;
+
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
